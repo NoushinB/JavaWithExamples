07d213118a2675bc51673b30536918ea05db03d1
noushinB <nou.babakhani@gmail.com>
2024-08-22 19:38:29 +0100

    Added CapitalizeStringExample

---
+++ src/main/java/org/noushin/examples/capitalize_string/CapitalizeStringExample.java
@@ -0,0 +1,20 @@
+package org.noushin.examples.capitalize_string;
+
+import java.util.Scanner;
+
+public class CapitalizeStringExample {
+
+    public static String capitalizeString(String input) {
+        return input.toUpperCase();
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);  // Create a Scanner object
+        System.out.println("Enter a string:");
+
+        String inputString = scanner.nextLine();
+        String capitalizeString = capitalizeString(inputString);
+        System.out.println("Capitalize string is: " + capitalizeString);
+
+    }
+}
