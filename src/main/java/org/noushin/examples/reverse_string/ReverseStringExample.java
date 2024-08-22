a4b050912dd17151e55c25045a947b2479ad648b
noushinB <nou.babakhani@gmail.com>
2024-08-22 18:52:56 +0100

    SwapStringsExample and ReverseStringExample main structure added

---
+++ src/main/java/org/noushin/examples/reverse_string/ReverseStringExample.java
@@ -0,0 +1,12 @@
+package org.noushin.examples.reverse_string;
+
+public class ReverseStringExample {
+
+    private String reverseString(String input) {
+        return input;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/org/noushin/examples/swap_two_strings/SwapStringsExample.java
@@ -0,0 +1,8 @@
+package org.noushin.examples.swap_two_strings;
+
+public class SwapStringsExample {
+
+    public static void main(String[] args) {
+
+    }
+}
