ea9c0f4116b8b7ac3ad0db7b17dd8fb8d29a0dfd
noushinB <nou.babakhani@gmail.com>
2024-08-22 19:12:10 +0100

    Finished ReverseStringExample

--- src/main/java/org/noushin/examples/reverse_string/ReverseStringExample.java
+++ src/main/java/org/noushin/examples/reverse_string/ReverseStringExample.java
@@ -1,12 +1,23 @@
 package org.noushin.examples.reverse_string;
 
+import java.util.Scanner;
+
 public class ReverseStringExample {
 
-    private String reverseString(String input) {
-        return input;
+    public static String reverseString(String input) {
+        StringBuilder result = new StringBuilder();  // Instead of result += input.charAt(i)
+        for (int i = input.length() - 1; i >= 0; i--) {
+            result.append(input.charAt(i));
+        }
+        return result.toString();
     }
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);  // Create a Scanner object
+        System.out.println("Enter a string:");
 
+        String inputString = scanner.nextLine();  // Read user input
+        String reversedString = reverseString(inputString);
+        System.out.println("Reversed string is: " + reversedString);  // Print reversed string
     }
 }
