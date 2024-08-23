2ecb84401ea4db88c7ea6df1a11ed54d2e30495d
noushinB <nou.babakhani@gmail.com>
2024-08-23 16:56:33 +0100

    Completed SwapStringsExample

--- src/main/java/org/noushin/examples/swap_two_strings/SwapStringsExample.java
+++ src/main/java/org/noushin/examples/swap_two_strings/SwapStringsExample.java
@@ -1,8 +1,29 @@
 package org.noushin.examples.swap_two_strings;
 
+import java.util.Scanner;
+
 public class SwapStringsExample {
 
-    public static void main(String[] args) {
+    public static String[] swapString(String inputString1, String inputString2) {
+        String temp = inputString1;
+        inputString1 = inputString2;
+        inputString2 = temp;
 
+        return new String[]{inputString1, inputString2};
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the first string:");
+        String inputFirstString = scanner.nextLine();
+
+        System.out.println("Enter a second string:");
+        String inputSecondString = scanner.nextLine();
+
+        String[] swappedStrings = swapString(inputFirstString, inputSecondString);
+
+        System.out.println("The first string is: " + swappedStrings[0]);
+        System.out.println("The second string is: " + swappedStrings[1]);
     }
 }
