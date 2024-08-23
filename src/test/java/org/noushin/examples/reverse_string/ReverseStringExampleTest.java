c951b6f8a16b745fee063a7209aee4acb5b39905
noushinB <nou.babakhani@gmail.com>
2024-08-23 17:18:22 +0100

    Added ReverseStringExample Unit Tests

---
+++ src/test/java/org/noushin/examples/reverse_string/ReverseStringExampleTest.java
@@ -0,0 +1,20 @@
+package org.noushin.examples.reverse_string;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ReverseStringExampleTest {
+
+    @Test
+    public void verifyReverseString(){
+        String testString= "unit test";
+        String expected="tset tinu";
+
+        String actual=ReverseStringExample.reverseString(testString);
+
+        assertEquals(expected,actual);
+
+    }
+
+}
