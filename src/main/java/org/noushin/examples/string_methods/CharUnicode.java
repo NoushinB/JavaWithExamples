ac0e1ff67f45a51982df98a2a8d7f97a186f8f76
noushinB <nou.babakhani@gmail.com>
2024-10-10 17:17:14 +0100

    findCountNumberOfUnicodeValue method Added to CharUnicode class

--- src/main/java/org/noushin/examples/string_methods/CharUnicode.java
+++ src/main/java/org/noushin/examples/string_methods/CharUnicode.java
@@ -4,26 +4,39 @@ import java.util.Scanner;
 
 public class CharUnicode {
 
-    public static void main(String args[]){
-        Scanner scanner=new Scanner(System.in);
+    public static void main(String args[]) {
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Enter a string: ");
-        String userInput=scanner.nextLine();
+        String userInput = scanner.nextLine();
         System.out.println("Enter an index: ");
-        int index=scanner.nextInt();
-        int unicode=findCharUnicode(index,userInput);
-        System.out.println("The Unicode of char in index ("+index+") is: "+ unicode );
+        int index = scanner.nextInt();
+        System.out.println("Enter an end index:");
+        int endIndex=scanner.nextInt();
+        int unicode = findCharUnicode(index, userInput);
+        System.out.println("The Unicode of char in index (" + index + ") is: " + unicode);
 
-        int unicodeBefore= findCodePointBefore(index,userInput);
-        System.out.println("The Unicode of char before index ("+index+") is: "+ unicodeBefore );
+        int unicodeBefore = findCodePointBefore(index, userInput);
+        System.out.println("The Unicode of char before index (" + index + ") is: " + unicodeBefore);
+
+
+        int numberOfUnicodValue= findCountNumberOfUnicodeValue(index,endIndex,userInput);
+        System.out.println("The number of Unicode values is: "+numberOfUnicodValue);
+
 
 
     }
-    public static int findCharUnicode(int index, String userInput){
+
+    public static int findCharUnicode(int index, String userInput) {
 
         return (userInput.codePointAt(index));
 
     }
-    public static int findCodePointBefore(int index, String userInput){
+
+    public static int findCodePointBefore(int index, String userInput) {
         return (userInput.codePointBefore(index));
+    }
+
+    public static int findCountNumberOfUnicodeValue(int beginIndex, int endIndex, String userInput) {
+        return (userInput.codePointCount(beginIndex, endIndex));
     }
 }
