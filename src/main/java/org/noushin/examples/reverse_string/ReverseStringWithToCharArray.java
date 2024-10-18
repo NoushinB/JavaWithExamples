e0707a0af26eb43c01139561dd793031abe9eded
noushinB <nou.babakhani@gmail.com>
2024-10-18 14:58:45 +0100

    reverseStringWithToCharArray method Added

--- src/main/java/org/noushin/examples/reverse_string/ReverseStringWithToCharArray.java
+++ src/main/java/org/noushin/examples/reverse_string/ReverseStringWithToCharArray.java
@@ -1,5 +1,27 @@
 package org.noushin.examples.reverse_string;
 
+import java.util.Scanner;
+
 public class ReverseStringWithToCharArray {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Please enter a string: ");
+        String userInput=scanner.nextLine();
+
+        String reverseString=reverseStringWithToCharArray(userInput);
+        System.out.println(reverseString);
+
+
+    }
+
+    public static String reverseStringWithToCharArray(String userInput){
+             char[]  myArray=userInput.toCharArray();
+             String reversString="";
+        for (int i=userInput.length()-1;i>=0;i--){
+           reversString+=myArray[i];
+        }
+        return reversString;
+
+    }
 
 }
