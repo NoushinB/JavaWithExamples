2012fd91776ac0e50bba4313887cff94af00d66b
noushinB <nou.babakhani@gmail.com>
2024-10-24 20:05:26 +0100

    SortAnArray class added

--- src/main/java/org/noushin/examples/array/LargestElementInArray.java
+++ src/main/java/org/noushin/examples/array/LargestElementInArray.java
@@ -9,8 +9,8 @@ public class LargestElementInArray {
         System.out.println("Enter the length of the Array:");
         int length = Integer.parseInt(scanner.nextLine());
         int[] array = new int[length];
-        for (int i=0; i<length; i++) {
-            System.out.println("Enter number " +( i + 1));
+        for (int i = 0; i < length; i++) {
+            System.out.println("Enter number " + (i + 1));
             array[i] = Integer.parseInt(scanner.nextLine());
         }
         int max = findLargestElementInArray(array);
--- src/main/java/org/noushin/examples/array/SortAnArray.java
+++ src/main/java/org/noushin/examples/array/SortAnArray.java
@@ -5,17 +5,17 @@ import java.util.Scanner;
 
 public class SortAnArray {
 
-    public static void main(String[] args){
-        Scanner scanner=new Scanner(System.in);
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Enter the length of the Array:");
-        int length=scanner.nextInt();
-        int[] array=new int[length];
-        for (int i=0;i<length;i++){
-            System.out.println("Enter number " +( i + 1));
-           array[i]=scanner.nextInt();
+        int length = scanner.nextInt();
+        int[] array = new int[length];
+        for (int i = 0; i < length; i++) {
+            System.out.println("Enter number " + (i + 1));
+            array[i] = scanner.nextInt();
         }
 
-     Arrays.sort(array);
+        Arrays.sort(array);
 
         for (int num : array) {
             System.out.print(num + " ");
