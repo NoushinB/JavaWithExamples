3aa32bef030f7322e3c6709e6c61e8e32c3a0c66
noushinB <nou.babakhani@gmail.com>
2024-10-26 15:41:48 +0100

    verifyPrimeNumber class added

---
+++ src/test/java/org/noushin/examples/PrimeNumberTest/PrimeNumberTest.java
@@ -0,0 +1,20 @@
+package org.noushin.examples.PrimeNumberTest;
+
+import org.junit.jupiter.api.Test;
+import org.noushin.examples.array.SortAnArray;
+import org.noushin.examples.prime_number.PrimeNumber;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class PrimeNumberTest {
+
+    @Test
+    public void   verifyPrimeNumber(){
+        int primeNum=13;
+
+        boolean expectResult=true;
+        boolean actualResult= PrimeNumber.checkPrimeNumber(primeNum);
+
+        assertEquals(expectResult,actualResult);
+    }
+}
