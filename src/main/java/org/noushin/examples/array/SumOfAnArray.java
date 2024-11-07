0f587b3d208868fb28935031c0b55dd3fde8675f
noushinB <nou.babakhani@gmail.com>
2024-11-07 15:23:16 +0000

    SumOfAnArray class added

---
+++ src/main/java/org/noushin/examples/array/SumOfAnArray.java
@@ -0,0 +1,32 @@
+package org.noushin.examples.array;
+
+import java.util.Scanner;
+
+public class SumOfAnArray {
+
+
+    public static void main(String[] args){
+       /* Scanner scanner=new Scanner(System.in);
+        System.out.println("Please input number of Array items");
+        int arrayItem=scanner.nextInt();
+        System.out.println("Array items");
+        int[] array= new int[arrayItem];
+        for(int i=0;i<arrayItem; i++){
+            System.out.println("Please input Array items index "+ i);
+            array[i]=scanner.nextInt();
+
+        }
+        System.out.println("Array items are: "+array);*/
+
+        int[] array={3,4,7};
+        int sumArray=0;
+
+        for (int j=0;j<array.length;j++){
+             sumArray+= array[j];
+
+        }
+
+        System.out.println(sumArray);
+
+    }
+}
