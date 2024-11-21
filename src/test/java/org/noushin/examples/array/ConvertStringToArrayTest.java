6cc8931787ba28385b4b8712fad9f2f19f5a7b89
noushinB <nou.babakhani@gmail.com>
2024-11-21 15:14:25 +0000

    verifyConvertStringToArray method added to ConvertStringToArrayTest.

--- src/test/java/org/noushin/examples/array/ConvertStringToArrayTest.java
+++ src/test/java/org/noushin/examples/array/ConvertStringToArrayTest.java
@@ -1,4 +1,18 @@
 package org.noushin.examples.array;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
 public class ConvertStringToArrayTest {
+
+
+    @Test
+    public void verifyConvertStringToArray() {
+        String string = "Noushin";
+        char[] actualArray = ConvertStringToArray.convertStringToArray(string);
+        char[] expectedArray = {'N', 'o', 'u', 's', 'h', 'i', 'n'};
+        assertArrayEquals(expectedArray, actualArray);
+    }
+
 }
