e9c10fb6a8fef50bed45cb64340c7c3e48fefc90
noushinB <nou.babakhani@gmail.com>
2024-11-26 20:18:07 +0000

    FinedSmallestElementInArray class improved.

--- src/main/java/org/noushin/examples/array/FinedSmallestElementInArray.java
+++ src/main/java/org/noushin/examples/array/FinedSmallestElementInArray.java
@@ -3,14 +3,18 @@ package org.noushin.examples.array;
 public class FinedSmallestElementInArray {
     public static void main(String[] args) {
         int[] array={33,1,55,2,0};
+        int small= smallestElement(array);
+        System.out.println("Smallest Element is: "+ small );
+    }
+
+    public static int smallestElement(int[] array){
         int small=array[0];
         for (int i=0;i<array.length;i++){
             if (small>array[i]){
                 small=array[i];
             }
-
-
         }
-        System.out.println("Smallest Element is: "+ small );
+
+        return small;
     }
 }
